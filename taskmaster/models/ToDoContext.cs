11cce6257ce847b9ed54ca3068aa83c4f47b633c
Aliyah Sanderson <aliyah.sanderson03@gmail.com>
2026-04-23 17:21:17 +0200

    Updated database context with notification table

--- taskmaster/models/ToDoContext.cs
+++ taskmaster/models/ToDoContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+
+
 namespace CloudSync.Models
 {
     public class ToDoContext : IdentityDbContext<User>
